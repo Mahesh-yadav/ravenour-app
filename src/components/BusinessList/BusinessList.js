4e4ca3aac67569796430f5878617d3df1d3538ad
Mahesh Yadav <mahesh.iitmandi@gmail.com>
2021-09-05 15:27:03 +0530

    Passing info using props

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,12 +4,9 @@ import Business from '../Business/Business';
 export default function BusinessList(props) {
   return (
     <div className="BusinessList">
-      <Business />
-      <Business />
-      <Business />
-      <Business />
-      <Business />
-      <Business />
+      {props.businesses.map((business, index) => (
+        <Business key={business.name + index} business={business} />
+      ))}
     </div>
   );
 }
